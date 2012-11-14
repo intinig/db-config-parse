87163a71c55c4c40f77934a99e510e4f2e8fd4ec
Giovanni Intini <giovanni@mikamai.com>
2012-11-14 17:43:28 +0100

    preparing for release

--- lib/db-config-parse.rb
+++ lib/db-config-parse.rb
@@ -14,10 +14,27 @@ module DBConfig
   end
 
   module Wordpress
-
-  end
+    def self.parse(config)
+      db_credentials = File.readlines config
+      configuration = db_credentials.find_all do |cr|
+        cr =~ /define\('DB_/
+      end.inject({}) do |acc, e|
+        f = e.match(/define\('(.*)', '(.*)'\).*$/)
+        acc.merge({f[1] => f[2]})
+      end
+      {:username => configuration['DB_USER'], :password => configuration['DB_PASSWORD'], :database => configuration['DB_HOST']}
+    end
 
   module Prestashop
-
+    def self.parse(config)
+      db_credentials = File.readlines config
+      configuration = db_credentials.find_all do |cr|
+        cr =~ /define\('_DB/
+      end.inject({}) do |acc, e|
+        f = e.match(/define\('(.*)', '(.*)'\).*$/)
+        acc.merge({f[1] => f[2]})
+      end
+      {:username => configuration['_DB_USER_'], :password => configuration['_DB_PASSWD_'], :database => configuration['_DB_NAME_']}
+    end
   end
 end
